2ee26b9bd061f2ca8aaf13e586eb71ca4c8621e7
Bogdan <40561032+BogdanPinchuk@users.noreply.github.com>
2019-09-08 23:26:38 +0300

    Add App2

---
+++ App2/DeleteDuplicate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    /// <summary>
+    /// Видалення дублыкатыв через розширений метод
+    /// </summary>
+    static class DeleteDuplicate
+    {
+        /// <summary>
+        /// видалення дублікатів
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static int[] MyDistinct(this int[] input)
+        {
+            if (input.Length == 0)
+            {
+                throw new Exception("Array is empty.");
+            }
+
+            // create new array
+            int[] array = new int[input.Length];
+
+            array[0] = input[0];
+
+            // counter
+            int count = 1;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] != input[i - 1])
+                {
+                    array[count++] = input[i];
+                }
+            }
+
+            int[] output = new int[count];
+
+            // this is not Linq
+            Array.Copy(array, 0, output, 0, count);
+
+            return output;
+        }
+
+        /// <summary>
+        /// показ результатів
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="s"></param>
+        public static void Show(this int[] input, string s)
+        {
+            var res = new StringBuilder(s.ToUpper() + ": int[] { ");
+            for (int i = 0; i < input.Length; i++)
+            {
+                res.Append(input[i] + ", ");
+            }
+
+            res = res.Remove(res.Length - 2, 2).Append(" }");
+
+            Console.WriteLine(res.ToString());
+        }
+
+    }
+}
---
+++ App2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Видалити дублікати із уже відсортованого масиву
+
+namespace App2
+{
+    class Program
+    {
+        static void Main()
+        {
+            // join unicode
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // goto - використовувати не бажано, але в даному випадку
+            // для ефективного повторення допустимо, так як запуск нового вікна 
+            // зміщує його, а рекурсія може призвести до переповнення стека
+            // якщо не використовувати алгоритм Дейкстри
+            Repeat:
+
+            // clear
+            Console.Clear();
+
+            // testing data
+            int[] input = new int[] { 1, 2, 3, 4, 4, 56 };
+
+            // use extention method
+            int[] output = input.MyDistinct();
+            // or use usual method 
+            int[] output0 = DeleteDuplicate.MyDistinct(input);
+
+            // result
+            input.Show("input");
+            Console.WriteLine();
+            output.Show("output");
+            output0.Show("output1");
+
+            #region Create data
+            // more interesting with use linq
+            // this is for create other array for testing
+            Random rnd = new Random();
+
+            input = new int[rnd.Next(2, 28)];
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = rnd.Next(0, 10);
+            }
+
+            // linq sorting
+            input = input.OrderBy(t => t).Select(t => t).ToArray();
+
+            #endregion
+
+            output = input.MyDistinct();
+
+            // show
+            Console.WriteLine();
+            input.Show("input");
+            output.Show("output");
+
+            #region Delay
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nRepeat again? [y, n]");
+            Console.ResetColor();
+            ConsoleKey key;
+
+            while (true)
+            {
+                key = Console.ReadKey(true).Key;
+                if (key == ConsoleKey.Y)
+                {
+                    goto Repeat;
+                }
+                else if (key == ConsoleKey.N ||
+                    key == ConsoleKey.Escape)
+                {
+                    //Environment.Exit(0);
+                    // or
+                    break;
+                }
+            } 
+            #endregion
+        }
+
+
+    }
+}
